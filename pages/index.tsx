c67f6d32505d7c55e1308cecd294e253a86193fc
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-09-29 08:45:44 +0200

    :alembic: Really.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,28 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import { PrismaClient, Testimonial } from "@prisma/client";
-import IndexComponents from "@/components/index";
+// import { PrismaClient, Testimonial } from "@prisma/client";
+// import IndexComponents from "@/components/index";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const prisma = new PrismaClient();
-  const testimonials = await prisma.testimonial.findMany();
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const prisma = new PrismaClient();
+//   const testimonials = await prisma.testimonial.findMany();
 
-  return { props: { testimonials } };
-};
+//   return { props: { testimonials } };
+// };
 
-type Props = { testimonials: Testimonial[] };
+// type Props = { testimonials: Testimonial[] };
 
-const Home: NextPage<Props> = (props: Props) => {
-  const { testimonials } = props;
+const Home: NextPage = () => {
+  // const { testimonials } = props;
   return (
     <>
       <Head>
-        <title>Peines de coeur {testimonials.length} </title>
+        <title>Peines de coeur</title>
       </Head>
       <div>
-        {testimonials.map((t) => (
+        {/* {testimonials.map((t) => (
           <IndexComponents.Testimonial testimonial={t} />
-        ))}
+        ))} */}
       </div>
     </>
   );
